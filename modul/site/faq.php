ef4f138aceae4bf356dd0a5a17e581f9d0ad4e7f
WinTeh <admin@dp-malish.com>
2021-12-15 16:04:41 +0200

    Start 0.0.0.0.2

--- modul/site/faq.php
+++ modul/site/faq.php
@@ -25,22 +25,66 @@ Opt::$main_content.='
 .f_item{
 background: #0ab59f;
 }
+.f_answ{
+display: none;
+}
 </style>
-
+<div class="f_item">
+    <div class="f_ques">
+3333
+    </div>
+    <div class="f_answ">
+4444
+    </div>
+</div>
 
 <div class="f_item">
     <div class="f_ques">
-
-
-
+1111
     </div>
     <div class="f_answ">
-
-
-
+2222
     </div>
-
 </div>
+<script type="text/javascript">
+
+//function faqAns(){alert(777)}
+//element.addEventListener(\'click\', faqAns);
+
+/*document.body.addEventListener(\'click\', function (evt) {
+    if (evt.target.className === \'f_ques\') {
+        alert(this)
+    }
+}, false);*/
+
+/*var faqdiv=document.getElementsByClassName("f_ques");
+faqdiv.addEventListener(\'click\', alert(5555));*/
+
+
+var buttonClick = document.getElementsByClassName("f_ques");
+/* можно так */
+   /*for (i=0; i< buttonClick.length; i++)
+   buttonClick[i].onclick = function()
+  {     alert("OK class");
+  };*/
+/* а лучше так*/ 
+[].slice.call(buttonClick).forEach(function(item) {
+        item.addEventListener("click",function(){
+           alert("OK class");
+           var papa= this.parentNode;
+           papa.style()
+           
+        });
+    });
+/* или так 
+[].forEach.call(document.querySelectorAll(\'.Buttons\'), function(item) {
+        item.addEventListener(\'click\', function() {
+           alert("OK class");
+        });
+    });*/
+
+
+</script>
 
 
 ';
