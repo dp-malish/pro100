f183af712cda8fd2bffadd83d2f6cd5eaee746ea
WinTeh <admin@dp-malish.com>
2021-12-16 14:23:43 +0200

    Start 0.0.0.0.3

--- modul/site/faq.php
+++ modul/site/faq.php
@@ -10,82 +10,201 @@ Opt::$title='FAQ SOCIAL SERVICE OF FINANCIAL MUTUAL SUPPORT';
 Opt::$description='FAQ SOCIAL SERVICE OF FINANCIAL MUTUAL SUPPORT';
 
 
-
-
-
-Opt::$main_content.='<main><article>
-<div class="ac"><p><br>FAQ<br><br></p></div>
-
-</article></main>';
-
-
-Opt::$main_content.='
-
-<style>
-.f_item{
-background: #0ab59f;
-}
-.f_answ{
-display: none;
-}
-</style>
-<div class="f_item">
-    <div class="f_ques">
-3333
-    </div>
-    <div class="f_answ">
-4444
-    </div>
-</div>
-
-<div class="f_item">
-    <div class="f_ques">
-1111
-    </div>
-    <div class="f_answ">
-2222
-    </div>
-</div>
-<script type="text/javascript">
-
-//function faqAns(){alert(777)}
-//element.addEventListener(\'click\', faqAns);
-
-/*document.body.addEventListener(\'click\', function (evt) {
-    if (evt.target.className === \'f_ques\') {
-        alert(this)
-    }
-}, false);*/
-
-/*var faqdiv=document.getElementsByClassName("f_ques");
-faqdiv.addEventListener(\'click\', alert(5555));*/
-
-
-var buttonClick = document.getElementsByClassName("f_ques");
-/* можно так */
-   /*for (i=0; i< buttonClick.length; i++)
-   buttonClick[i].onclick = function()
-  {     alert("OK class");
-  };*/
-/* а лучше так*/ 
-[].slice.call(buttonClick).forEach(function(item) {
-        item.addEventListener("click",function(){
-           alert("OK class");
-           var papa= this.parentNode;
-           papa.style()
-           
-        });
-    });
-/* или так 
-[].forEach.call(document.querySelectorAll(\'.Buttons\'), function(item) {
-        item.addEventListener(\'click\', function() {
-           alert("OK class");
-        });
-    });*/
-
-
-</script>
-
-
-';
+Opt::$main_content.='<main><div class="d_defs b_fon">
+<article>
+<h2>FAQ</h2>
+<div class="maxw">
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">1. Where is the affiliate link for the invitation?</summary>
+                <span class="five_">
+                <p>The affiliate link can be found on the main page of your personal account in the top menu.</p>
+                </span>
+                
+            </details>
+        </section>
+        </div>
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">2. What is an overflow operation?</summary>
+                <span class="five_">
+                <p>If your first line is already busy, the new member registers through your link to your second line. If your second line is also busy, he/she will go to your third line and so on.</p>
+                </span>
+                
+            </details>
+        </section>
+        </div>
+        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    3. How do I change my payment and contact details?
+                </summary>
+                <span class="five_">
+                <p>
+                    Сlick on your avatar and go to "Profile" In your personal profile in the top right-hand corner.
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    4. What should I do if I don\'t have a supervisor?
+                </summary>
+                <span class="five_">
+                <p>
+                    When you register, the system will automatically select a supervisor for you.
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    5. Which payment system is used in the system?
+                </summary>
+                <span class="five_">
+                <p>
+                    The system works with the Perfect Money payment system.
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    6. How  much quickly do funds arrive after a payout has been ordered?
+                </summary>
+                <span class="five_">
+                <p>
+                    Instantly upon request.
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    7. Is there a withdrawal fee?
+                </summary>
+                <span class="five_">
+                <p>
+                    Only the commission of the "Perfect Money" payment system.
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    8.  Who is the curator?
+                </summary>
+                <span class="five_">
+                <p>
+                    This is the user under which you registered.
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    9. How to raise the level?
+                </summary>
+                <span class="five_">
+                <p>
+                    You need to top up your balance, then go to "Level up".
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    10. The level is paid for, what do I do next?
+                </summary>
+                <span class="five_">
+                <p>
+                    Next, begin to work actively with your contacts.
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    11. What happens if I don\'t take the first level after registering?
+                </summary>
+                <span class="five_">
+                <p>Why did you register in the system? You are just wasting your time, while you are thinking, other participants are already receiving cash gifts.
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>        
+        <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                    12. How many people can you invite?
+                </summary>
+                <span class="five_">
+                <p>You can invite as many people as you like. Your first line is limited to 3 referrals. If your first line is already taken, a new referral will stand up as an overflow to your second line. The number of people you can invite is limited only by your ability to invite them.  
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>
+ 
+ 
+  <div class="maxw8">
+        <section>
+            <details class="five_">
+                <summary class="b">
+                13. I have forgotten the password to my account. What can I do?
+                </summary>
+                <span class="five_">
+                <p>Use the password recovery form. A new password will be sent to the email address you provided during registration.
+                </p>
+                </span>
+                
+            </details>
+        </section>
+        </div>       
+         
+ </div>
+ 
+ 
+ 
+</article></div></main>';
 
