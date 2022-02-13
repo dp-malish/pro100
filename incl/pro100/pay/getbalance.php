4c7ae3abcbc462b19e92c11e7e28342aad6e405b
Дом <admin@dp-malish.com>
2022-02-13 10:59:21 +0200

    First step 0.0.0.1.2

--- incl/pro100/pay/getbalance.php
+++ incl/pro100/pay/getbalance.php
@@ -7,12 +7,12 @@ use lib\Def\SQLi;
 
 class GetBalance{
 
-    public $all_p=0;
+    public $all_p=0;//Вся прибыль (Вычитать стоимость активации на уровнях)
     public $all_m=0;
     public $reserve=0;
     public $free=0;
     public $free_level=0;//Саободные средства сумма по уровню
-    public $free_limit=[];//Саободные средства на уровне
+    public $free_limit=[];//Всего возможно средства на уровне именно пользователя с учётом всех выщетов
     public $free_limit_residue=0;//Общий возможный доход/остаток именно пользователя с учётом всех выщетов
 
     public $current_limit=0;//Затраты на овышения уровня
@@ -26,12 +26,16 @@ class GetBalance{
 
         $txt='';
 
+        $this->all_p=$res['sp'];
         $partners=[0,3,9,27,81];
 
         for($index=1;$index<=User\User::$arrDBUser['level'];$index++){
             //100*Def100\OptCab::COMMISSION
-            $txt.=$index.' - '.  Def100\OptCab::LEVEL_COST[$index] .'-'.(Def100\OptCab::LEVEL_COST[$index]*$partners[$index]-(Def100\OptCab::LEVEL_COST[$index]*$partners[$index]/100*Def100\OptCab::COMMISSION)).  '<br>';
+            //$txt.=$index.' - '.  Def100\OptCab::LEVEL_COST[$index] .'-'.(Def100\OptCab::LEVEL_COST[$index]*$partners[$index]-(Def100\OptCab::LEVEL_COST[$index]*$partners[$index]/100*Def100\OptCab::COMMISSION)).  '<br>';
 
+
+            //Всего плучено в проекте (таблица sql минус активация уровней соглавсно твоему)
+            $this->all_p-=Def100\OptCab::LEVEL_COST[$index];
             //Саободные средства на уровне
             $this->free_limit[$index]=Def100\OptCab::LEVEL_COST[$index]*$partners[$index]-(Def100\OptCab::LEVEL_COST[$index]*$partners[$index]/100*Def100\OptCab::COMMISSION);
             //Саободные средства сумма по уровню
@@ -52,13 +56,17 @@ class GetBalance{
 
 
 
-        return $res['uid'].'<br>'
-            .$res['level'].'<br>'
+        return '<br>Uid: '.$res['uid'].'<br>'.
+            '<br>Level: '.$res['level'].'<br>'
             //$res['bal'].
-            .'<br>Всего плучено в проекте: '.$res['sp']
-            .'<br>Всего выведено из проекта:'.$res['sm'].'<br><br><br><br>'
-            .$txt.
-            '<br><br>Общий доход на уровне: '.($index-1). ' - '.$this->free_level
+            .'<br>Всего плучено в проекте (таблица sql): '.$res['sp']
+            .'<br>Всего плучено в проекте (таблица sql минус активация уровней соглавсно твоему): '.$this->all_p
+            .'<br>Всего выведено из проекта:'.$res['sm'].'<br><br><br><br>'.
+            '<br>Всего возможно средства на уровне 1: '.$this->free_limit[1].
+            '<br>Саободные средства на уровне 2: '.$this->free_limit[2].
+            '<br>Саободные средства на уровне 3: '.$this->free_limit[3].
+            '<br>Саободные средства на уровне 4: '.$this->free_limit[4].
+            '<br><br>Общий доход на уровне: '.$this->free_level
 
             .'<br><br>Общий возможный доход/остаток: '.$this->free_limit_residue.'<br><br>'.User\User::$arrDBUser['level'];
 
